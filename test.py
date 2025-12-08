d88465ba67768f8e4e391a91074ad1ca9a6ee039
Karthikeya <kamalkarthikeyakumar@gmail.com>
2025-12-08 21:48:52 +0530

    name change

---
+++ test.py
@@ -0,0 +1,16 @@
+from recommender.recommender import group_recommend
+places = [
+    {"name": "Goa", "tags": ["beach", "nightlife", "food"]},
+    {"name": "Manali", "tags": ["mountains", "nature", "adventure"]},
+    {"name": "Jaipur", "tags": ["history", "food", "culture"]}
+]
+
+group_users = [
+    ["beach", "food"],
+    ["beach", "nightlife"]
+]
+
+recommendations = group_recommend(group_users, places)
+
+for r in recommendations:
+    print(r)
