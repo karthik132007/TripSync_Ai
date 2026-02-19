009cb9c5a556d1443df2111616be7a99baa741ee
Karthikeya <kamalkarthikeyakumar@gmail.com>
2026-02-19 15:17:09 +0530

    all set from model

---
+++ db/get_from_db.py
@@ -0,0 +1,34 @@
+import psycopg2
+import pandas as pd
+from dotenv import load_dotenv
+import os
+load_dotenv()
+conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        port=os.getenv("DB_PORT")
+    )
+cur = conn.cursor()
+
+def get_hotels():
+    try:
+        rows=cur.execute("select * from hotel_features;")
+        rows = cur.fetchall()
+        rows = pd.DataFrame(rows)
+        return rows
+    except Exception as e:
+        print(e)
+        conn.rollback()
+
+def get_places():
+
+    try:
+        rows=cur.execute("select * from place_features;")
+        rows = cur.fetchall()
+        rows = pd.DataFrame(rows)
+        return rows
+    except Exception as e:
+        print(e)
+        conn.rollback()
